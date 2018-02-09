7f252e87ecd9922be112f2664149c9a7814863a4
cyio <icaner@qq.com>
2018-02-10 00:07:47 +0800

    use port 8443

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -109,7 +109,7 @@ export default {
     if (change) {
       this.selectedChange = change
     }
-    let wsUrl = 'wss://' + window.location.host
+    let wsUrl = 'wss://' + window.location.host + ':8443'
     if (window.location.hostname === 'localhost') {
       wsUrl = 'ws://localhost:8083'
     }
